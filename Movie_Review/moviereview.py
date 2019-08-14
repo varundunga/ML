4fa51b572df1e3e28688daca65944fd4d5ff135c
Varun Teja Dunga <vdunga@evoketechnologies.com>
2019-08-14 20:26:26 +0530

    a

--- Movie_Review/moviereview.py
+++ Movie_Review/moviereview.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#
-dataset=pd.read_csv('311_Service_Requests_from_2010_to_Present.csv')
-data=dataset[['Complaint Type','City']]
+from pathlib import Path
+home = str(Path.home())
+movies_header=['MovieID','Title','Genres']
+movies=pd.read_csv(home + '\\Machine learning\\Movie_Review\\movies.dat',sep='::',header=None,names=movies_header)
+users_header=['UserID','Gender','Age','Occupation','Zip-code']
+users=pd.read_csv(home + '\\Machine learning\\Movie_Review\\users.dat',sep='::',header=None,names=users_header)
+ratings_header=['UserID','MovieID','Rating','Timestamp']
+ratings=pd.read_csv(home + '\\Machine learning\\Movie_Review\\ratings.dat',sep='::',header=None,names=ratings_header)
